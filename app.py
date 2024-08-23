12987e8066f8a28527fa6aa254b81cec3b789529
afomike <temidayoafote@gmail.com>
2024-08-23 23:14:37 +0100

    new

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 app = Flask(__name__)
 
 # Load the trained model and encoders once
-model = joblib.load('model/degree_class_predictive_model.joblib')
+model = joblib.load('model/Obesit_Risk_Using_Behavioral_and_Dietary_Pattern.joblib')
 one_hot_encoder = joblib.load('model/one_hot_encoder.joblib')
 label_encoders = joblib.load('model/label_encoders.joblib')
 
@@ -15,12 +15,8 @@ def preprocess_input(data):
     # Convert input data to a DataFrame
     df = pd.DataFrame(data, index=[0])
 
-    # Convert relevant columns to numeric, fill missing values with 0
-    df['PREV_GPA'] = pd.to_numeric(df['PREV_GPA'], errors='coerce').fillna(0)
-    df['GPA'] = pd.to_numeric(df['GPA'], errors='coerce').fillna(0)
-
     # Label Encoding for categorical features
-    label_encode_features = ['AVG_GRADE_HS']
+    label_encode_features = ['Gender', 'family_history_with_overweight']
     for column in label_encode_features:
         if column in df.columns:
             le = label_encoders[column]
@@ -28,11 +24,7 @@ def preprocess_input(data):
             df[column] = df[column].apply(lambda x: le.transform([x])[0] if x in known_classes else -1)  # Using -1 for unknown
 
     # One-Hot Encoding for categorical features
-    one_hot_features = [
-        'SCHOOL_TYPE', 'GAP_BEFORE_DEGREE', 
-        'MAJOR', 'STUDY_SCHEDULE',
-        'PART_TIME_JOB', 'MOTIVATION', 'STRESS_MANAGEMENT'
-    ]
+    one_hot_features = ['CALC', 'FAVC', 'SMOKE', 'CAEC']
     if all(feature in df.columns for feature in one_hot_features):
         # Use the handle_unknown='ignore' parameter to ignore unknown categories
         one_hot_encoded = one_hot_encoder.transform(df[one_hot_features])
@@ -56,11 +48,8 @@ def predict():
         data = request.form.to_dict()
 
         # Check for required fields
-        required_fields = [
-            'PREV_GPA', 'GPA', 'SCHOOL_TYPE', 'GAP_BEFORE_DEGREE', 
-            'MAJOR', 'STUDY_SCHEDULE', 'PART_TIME_JOB', 
-            'MOTIVATION', 'STRESS_MANAGEMENT', 'AVG_GRADE_HS'
-        ]
+        required_fields = ['Age', 'Gender', 'CALC', 'FAVC', 'FCVC', 'NCP', 'SMOKE', 'CH2O',
+       'family_history_with_overweight', 'FAF', 'CAEC']
         
         missing_fields = [field for field in required_fields if field not in data]
         if missing_fields:
@@ -69,7 +58,25 @@ def predict():
         preprocessed_data = preprocess_input(data)
         prediction = model.predict(preprocessed_data)
         prediction_result = prediction[0]
-        return render_template('index.html', prediction=f'Predicted Degree Class: {prediction_result}')
+
+        if prediction_result == 0:
+            prediction_result ="Insufficient Weight"
+        elif prediction_result == 1:
+            prediction_result="Normal Weight"
+        elif prediction_result == 2:
+            prediction_result="Obesity Type I"
+        elif prediction_result == 3:
+            prediction_result="Obesity Type II"
+        elif prediction_result == 4:
+            prediction_result="Obesity Type III"
+        elif prediction_result == 5:
+            prediction_result="Overweight Level I"
+        elif prediction_result == 6:
+            prediction_result="Overweight Level II"
+        else:
+            prediction_result="Unknown label"
+
+        return render_template('index.html', prediction=f'Your are likely to be: {prediction_result}')
     except Exception as e:
         return render_template('index.html', prediction=f"Error: {str(e)}")
 
